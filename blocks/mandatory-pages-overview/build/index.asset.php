6032a009d6d9eeace5539ac4f256719129c61ee0
Tsjippy <ewaldha@hotmail.com>
2024-10-04 12:59:59 +0100

    skip:

--- blocks/mandatory-pages-overview/build/index.asset.php
+++ blocks/mandatory-pages-overview/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e28a42104f098d8b9e7a');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2d99b0352b5b5c373b7b');
--- blocks/mandatory-settings/build/index.asset.php
+++ blocks/mandatory-settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element'), 'version' => '83776d9a07e9bd7808a2');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post'), 'version' => '97a2f9a8c7135a3d9b08');
