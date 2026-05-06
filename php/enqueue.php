18bcfe9a0609b705dd7ca74d3c52d5cfa5a13ef6
Tsjippy <ewaldha@hotmail.com>
2026-05-06 10:54:12 +0200

    skip:

--- php/enqueue.php
+++ php/enqueue.php
@@ -6,9 +6,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_action( 'wp_enqueue_scripts', __NAMESPACE__.'\registerMandatoryScripts');
+add_action( 'wp_enqueue_scripts', __NAMESPACE__.'\registerMandatoryScripts', 20);
 
 function registerMandatoryScripts(){
     wp_register_style('tsjippy_mandatory_style', TSJIPPY\pathToUrl(PLUGINPATH.'css/mandatory.min.css'), array(), PLUGINVERSION);
-    wp_register_script('tsjippy_mandatory_script', TSJIPPY\pathToUrl(PLUGINPATH.'js/mandatory.min.js'), array('tsjippy_formsubmit_script'), PLUGINVERSION,true);
+    wp_register_script('tsjippy_mandatory_script', TSJIPPY\pathToUrl(PLUGINPATH.'js/mandatory.min.js'), array('tsjippy_formsubmit_script'), PLUGINVERSION, true);
 }
