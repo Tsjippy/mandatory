e4b5716b559d1df273a6a0a2de1f5452728ee67d
Tsjippy <ewaldha@hotmail.com>
2026-07-04 09:00:04 +0200

    skip:

--- blocks/mandatory-settings/build/index.asset.php
+++ blocks/mandatory-settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post'), 'version' => '5c9730acee1b5bea47e0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor'), 'version' => '24b4998653b1918a92d1');
