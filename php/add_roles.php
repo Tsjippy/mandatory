3dd328217deb401da0d360bc1f2612ea0c3315fa
Tsjippy <ewaldha@hotmail.com>
2026-07-12 12:40:48 +0200

    skip:

--- php/add_roles.php
+++ php/add_roles.php
@@ -9,6 +9,12 @@ if (! defined('ABSPATH')) {
 }
 
 add_filter('tsjippy-user-management-role-description', __NAMESPACE__ . '\roleDescription', 10, 2);
+/**
+ * Filters the role description
+ * 
+ * @param string $description  The description of a user role
+ * @param string $role         The role slug
+ */
 function roleDescription($description, $role)
 {
     if ($role == 'no_man_docs') {
